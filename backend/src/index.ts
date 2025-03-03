0527aac691a21b0726dd86f275fb1241d293c9c5
Shalom Salazar <shalomsalazar2002@gmail.com>
2025-03-02 22:25:56 -0600

    auth routes and controllers added

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+const app = express();
+
+app.use(cookieParser()); // for parsing cookies
+app.use(express.json()); // for parsing application/json
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV !== "development") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+}
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
---
+++ backend/src/routes/auth.route.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  login,
+  logout,
+  signup,
+  getMe,
+} from "../controllers/auth.controller.js";
+
+import protectRoute from "../middleware/protectroute.js";
+
+const router = express.Router();
+
+router.get("/me", protectRoute, getMe);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+export default router;
